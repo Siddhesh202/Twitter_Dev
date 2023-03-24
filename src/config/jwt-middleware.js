1f4dafe9736e38eb6c71b77b61b02acef0eaac32
Siddhesh202 <siddheshpisal76@gmail.com>
2023-03-25 02:36:39 +0530

    Added passport logic

--- src/config/jwt-middleware.js
+++ src/config/jwt-middleware.js
@@ -1,7 +1,7 @@
 import JWT from "passport-jwt";
 import User from "../models/user.js";
 
-const JwtStrategy = new JWT.Strategy;
+const JwtStrategy = JWT.Strategy;
 const ExtractJwt = JWT.ExtractJwt;
 
 const opts = {
@@ -10,12 +10,17 @@ const opts = {
 }
 
 export const passportAuth = (passport) => {
-    passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
-        const user = await User.findById(jwt_payload.id);
-        if(!user){
-            done(null, false);
-        }else {
-            done(null, user)
-        }
-    }))
+    try {
+        passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+            const user = await User.findById(jwt_payload.id);
+            if(!user){
+                done(null, false);
+            }else {
+                done(null, user)
+            }
+        }))
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import passport from 'passport'
 import {connect} from './config/database.js'
 import apiRoutes from './routes/index.js'
+import { passportAuth } from "./config/jwt-middleware.js"
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
+
+app.use(passport.initialize());
+passportAuth(passport);
 
 app.listen(3000,async () => {
     console.log(`Server is up and running on PORT : 3000`);
